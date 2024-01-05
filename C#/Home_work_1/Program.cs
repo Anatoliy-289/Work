b56d63773f22a42be4953b994bc8a291fb3e4b1f
Anatoliy Turanskiy <anatol911@yandex.ru>
2024-01-05 20:41:11 +0500

    L

--- C#/Home_work_1/Program.cs
+++ C#/Home_work_1/Program.cs
@@ -34,3 +34,22 @@
 //{
     //Console.Write($"Точка находится в 4 координатной четверти");
 //}
+
+
+
+
+
+// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int num = 27;
+if (num > 10 && num < 99){
+    int first_digit = num/10;
+    int second_digit = num%10;{
+        if (first_digit > second_digit){
+            Console.Write($"{first_digit}");
+        }
+        else
+            Console.Write($"{second_digit}");
+    }
+}
+else
+    Console.Write($"Число должно быть больше 10 и меньше 99");
