e069ca736e658a2f09b3a108b3d8ff5fc9561db8
Anatoliy Turanskiy <anatol911@yandex.ru>
2024-01-29 21:45:27 +0500

    Добавлено решение задач

---
+++ C#/Home_work_3/Program.cs
@@ -0,0 +1,126 @@
+﻿// Задача 1. Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+
+
+//string str = q;
+//Console.Write($"Введите целое число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+
+//int sum = 0;
+
+//if (number > 9 && number.Convert.ToInt(q))
+    //{ 
+        //sum = sum + number % 10;
+        //number = number / 10;
+
+        //Console.Write($"Число четное или сумма чисел четная. Программа завершила свою работу.");
+    //}
+
+    //else        
+        //{
+        //Console.Write($"Число нечетное или сумма чисел нечетная. Введите другое число.");
+        //}
+
+
+
+
+
+// Задача 2. Задайте массив, заполненный случайными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве.
+
+
+
+//int [] CreateThreeDigitNumbersArray(int size, int min, int max)
+//{
+    //int [] array = new int [size];
+    //for (int i = 0; i < size; i++)
+        //array [i] = new Random().Next(min, max+1);
+        
+    //return array;
+//}
+
+//void ShowArray (int [] array)
+//{
+    //foreach (int e in array)
+    //{
+        //Console.Write($"{e} ");
+    //}
+//}
+
+//Console.WriteLine("Введите размер массива: ");
+//int size = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите минимальное число: ");
+//int min = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите максимальное число: ");
+//int max = Convert.ToInt32(Console.ReadLine());
+
+//int [] array = CreateThreeDigitNumbersArray(size,min,max);
+
+//ShowArray(array);
+
+
+//void QuantityEvenNumbers(int [] array)
+    //{
+        //int count = 0;
+        //for (int i = 0; i < array.Length; i++)
+            //{
+                //if (array[i] % 2 == 0)
+                //{
+                    //count = count + 1;
+                //}
+            //}
+        //Console.Write($"количество четных чисел в массиве: {count}");
+//}
+
+//QuantityEvenNumbers(array);
+
+
+
+
+
+// Задача 3. Заполните массив на N (вводится с консоли, не более 8) cлучайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из массива.
+// Старший разряд числа находится на 0-м индексе, младший - на последнем.
+
+
+
+
+
+//int number = 0;
+
+//int [] CreateRandomArray(int size, int min, int max)
+//{
+    //int [] array = new int [size];
+    //for (int i = 0; i < size; i++);
+        //{
+        //array [i] = new Random().Next(min; max+1);
+        //}
+        
+    //return array;
+//}
+
+
+//void ShowNumber(int [] array)
+    //{
+        //for (int i = 0; i < array.Lehgth; i++);
+            //{
+               //array [i] = Convert.ToString()
+               //number = number.Convert.ToString() + array[i];
+            //}
+            
+            //Console.Write($"number");
+    //}
+
+//Console.WriteLine("Введите восемь случайных чисел от 0 до 9: ");
+//int size = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите минимальное число: ");
+//int min = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите максимальное число: ");
+//int max = Convert.ToInt32(Console.ReadLine());
+
+//int [] array = CreateRandomArray(size, min, max);
+
+//ShowArray(array);
