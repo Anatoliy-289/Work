8d3170692b6849e8748c9fffb7fb0b570d32f1cf
Anatoliy Turanskiy <anatol911@yandex.ru>
2024-02-07 22:57:40 +0500

    Добавлено решение задач

---
+++ C#/Home_work_5/Program.cs
@@ -0,0 +1,104 @@
+﻿// Задача 1. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы
+
+
+
+//void ShowDigits(int m, int n, )
+//{
+    //if (num > m && nim < n);
+    //{
+        //return;
+    //}        
+        
+    //ShowDigits(m, n);
+    //Console.Write(num + " ");   
+        
+    //Console.WriteLine("Введите значение m");
+    //int m = Convert.ToInt32(Console.Readline());
+    //Console.WriteLine("Введите значение n");
+    //int n = Convert.ToInt32(Console.RedLine());
+
+//}
+
+//ShowDigits(4, 8);
+
+
+
+
+
+// Задача. 2 Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+
+
+//int AkkermanFunc(int m, int n);
+//{
+    //if (m = 0)
+    //{
+        //return (n + 1);
+    //}
+    
+    //elif (m > 0 && n = 0)
+    //{
+        //return (AkkermanFunc(m - 1, 1));
+    //}
+
+    //else (m > 0 && n > 0)
+    //{
+        //return (AkkermanFunc(m - 1, AkkermanFunc(m, n - 1)));
+    //}
+//}
+
+//void AkkermanFunc(int m, int n);
+//{
+    //Console.Write(AkkermanFunc(m, n));
+//}
+
+
+
+//Console.WriteLine ("Введите число m: ");
+//int size = Convert.ToInt32(Console.Readline());
+//Console.WriteLine ("Введите число n: ");
+//int min = Convert.ToInt32(Console.ReadLine());
+
+//AkkermanFunс(m, n);
+
+
+
+
+
+// Задача 3. Задайте произвольный массив. Выведите его элементы, начиная с конца.
+// Использовать рекурсию. Не использовать циклы.
+
+
+
+//int [] CreateArray(int size, int min, int max);
+//{
+    //int [] array = new int [size];
+    //for (int i = 0; i < size; i++)
+    //{
+        //array [i] = new.Random().Next(min; max+1);
+    //} 
+    //return array;
+//}
+
+//void ShowDigits (int [] array);
+//{
+    //if (num = min)
+    //{
+        //return;
+    //}
+    //ShowDigits (max-1);
+    //Console.Write (num + " ");
+//}
+
+//Console.WriteLine ("Введите размер массива: ");
+//int size = Convert.ToInt32(Console.Readline());
+//Console.WriteLine ("Введите минимальное значение: ");
+//int min = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine ("Введите максимальное значение: ");
+//int max = Convert.ToInt32(Console.Redline());
+
+//int [] array = CreateArray(size, min, max);
+
+//ShowDigits(array);
